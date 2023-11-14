b643faac40cc3ca7a854099f1a9b9f70b0c07726
johnny-hui <johnny_hui@live.com>
2023-11-13 20:09:29 -0800

    Testing TCP destination port v2

--- victim/victim_main.py
+++ victim/victim_main.py
@@ -287,9 +287,9 @@ if __name__ == '__main__':
                         count = get_packet_count(client_socket)
 
                         if constants.SOURCE_PORT_FIELD in choices:
-                            received_packets = sniff(filter="tcp and dst host {} and dst port {} and "
+                            received_packets = sniff(filter="tcp and src host {} and dst host {} and dst port {} and "
                                                             "(tcp[13] & 0x004 == 0)"  # tcp[13] offset RST flag (0x004)
-                                                     .format(source_ip, source_port),
+                                                     .format(client_address[0], source_ip, source_port),
                                                      count=count)
 
                         elif constants.DESTINATION_ADDRESS_FIELD in choices:
